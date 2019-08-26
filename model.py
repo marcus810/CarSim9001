9fecb677b34ae012d9bc9f540c242014484ddd23
marcus810 <marcus@jensenpost.dk>
2019-08-26 12:31:19 +0200

    Klassen Tank done

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ class Car(object):
     pass
 
 class Wheel(object):
+
     pass
 
 class Engine(object):
@@ -13,4 +14,19 @@ class Gearbox(object):
     pass
 
 class Tank(object):
+
+    def __init__(self):
+        Tank.capacity = 100
+        Tank.contents = 100
+
+    def remove(self, amount):
+        self.contents = self.contents - amount
+        if self.contents < 0:
+            self.contents = 0
+        return self.contents
+
+
+    def refuel(self):
+        self.contents = self.capacity
+        return self.contents
     pass
